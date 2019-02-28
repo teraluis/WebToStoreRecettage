d41c809f14c36e208db9d7c4dfd9263fa697d4c3
Luis Manresa <luismanresaramirez@gmail.com>
2019-02-28 14:54:07 +0100

    marche pas encore

--- test.js
+++ test.js
@@ -86,10 +86,10 @@ function sanitizeHTML(strings) {
 }
 var user_data=[];
 function retourne_position(){
-  //console.log(user_data);
   var x = document.getElementById("inscription");
-  document.getElementById("magasin_selectione").innerHTML = "magasin selectione "+user_data[2];
+  document.getElementById("magasin_selectione").innerHTML = "magasin selectione "+user_data[0];
   x.style.display = "block";
+  window.scrollTo(0,document.querySelector("#inscription").scrollHeight);
   
 }
 function initMap(){
@@ -104,8 +104,8 @@ function initMap(){
   map.data.setStyle(feature => {
     return {
       icon: {
-        url: `https://www.angeleyes-eyewear.com/wp-content/themes/angel-eyes-vitrine/assets/img/favicon.png`,
-        scaledSize: new google.maps.Size(15, 15)
+        url: `img/vinyl_logo.png`,
+        scaledSize: new google.maps.Size(30, 30)
       }
     };
   });
@@ -135,14 +135,17 @@ function initMap(){
     const phone = event.feature.getProperty('telephone');
     const position = event.feature.getGeometry().get();
     user_data =[];
-    user_data.push(position);
+    user_data.push(opticien);
+    user_data.push(adresse);
+    user_data.push(ville);
     user_data.push(phone);
-    user_data.push(name);
     const content = sanitizeHTML`
-      <div style="width:200px; margin-bottom:20px;">
+      <div style="width:300px; margin-bottom:20px;font-size:1em">
+      <br>
         <p><b>Opticien:</b> ${opticien}</p>
-        <p><b>Adresse:</b> ${adresse}<br/><b>Code Postal :</b> ${postal_code}</p><p><b>Ville:</b> ${ville} </p>
-        <a href="#inscription" type="button" onclick='retourne_position()' class="btn btn-primary">Sélectionner</a>
+        <p><b>Adresse:</b> ${adresse}<br/><b>Code Postal :</b> ${postal_code} <br> <b>Ville:</b> ${ville} </p>
+        <br>
+        <button onclick='retourne_position()' class="button" >Sélectionner</button>
       </div>
     `;
 
@@ -151,6 +154,8 @@ function initMap(){
     infoWindow.open(map);
   });
   }
+
+
 $(document).ready(function(){
 var today = new Date();
 var dd = today.getDate()+3;
@@ -182,31 +187,46 @@ document.getElementById('date').setAttribute("min",today);
     var geocode = codeAddress(address);
 
   });
-   var fromulaire = $('#inscription');
+   
   function submitWebToStore() {
+      var fromulaire = $('#inscription');
+      var champs = {};
+      let tmp = user_data[0];
+      tmp = escapeHtml(tmp);
+      console.log(tmp);
+      champs['opticien']=tmp;
+      var tmp2 = escapeHtml(user_data[1]);
+      champs['adresse']=tmp2;
+      champs['ville']=user_data[2];
+      champs['phone']=user_data[3];
+
       var form = fromulaire.serialize();
+
       
       $.ajax({
           type: "POST",
           dataType: "json",
-          url: "prisse_rdv.php",//url: $('#needs-validation').attr( 'action' ),
-          data: form,
+          url: "prisse_rdv.php",
+          data: form+"&opticien="+champs['opticien']+"&direcion="+champs['adresse']+"&ciudad="+champs['ville'],
           reponseType:'json',
           beforeSend: function () {
-            /* silence is golden */
+            $("#chargement").show();
           },
           success: function (data) {
+              $("#chargement").hide();
               var obj=data;
               console.log(obj);
-/*              var resultat = JSON.stringify(obj);
-              resultat = eval(resultat);
-              resultat =JSON.parse(resultat);*/
-              
+              $("#recapitulatif").text("vous serez attendu à "+obj['adresse']+" le "+obj['date']+" chez "+obj['opticien']);
+              let modal = document.getElementById("modal-body");
+              modal.style.display="none";
+              let modal2 = document.getElementById("modal_body2");
+              modal2.style.display="block";
           },
           complete: function () {
-            
+            $("#chargement").hide();
           },
           error: function (jqXHR, textStatus, errorThrown) {
+              $("#chargement").hide();
               console.log(jqXHR + " :: " + textStatus + " :: " + errorThrown);
           }
       });
@@ -220,14 +240,61 @@ document.getElementById('date').setAttribute("min",today);
       && $("#telephone").val()!=""
       && $("#date").val()!=""
       ){
-    let modal = document.getElementById("modal-body");
-    modal.style.display="none";
-    let modal2 = document.getElementById("modal_body2");
-    modal2.style.display="block";
-    submitWebToStore();
+      let date =$("#date").val();
+      if(validation_jour(date)==true){
+        let modal = document.getElementById("modal-body");
+        modal.style.display="none";
+        $("#chargement").show();
+        submitWebToStore();
+        return false;
+      }else {
+        alert("la date ne dois pas être un dimanche ou un lundi et elle doit être 3 jours plus tard à compter de l date d'aujord'hui")
+      }
+
     }else {
       alert("veuillez completer touts les champs  svp.");
     }
-
+    
     });
 });
+function escapeHtml(text) {
+  return text
+      .replace(/&amp;/g, "")
+      .replace(/&rsquo;/g, "")
+      .replace(/&amp;/g, "")
+      .replace(/#8211;/g, "")
+      .replace(/&/g, "ET")
+      .replace(/&#039;/g, "");
+}
+function validation_jour(date){
+  var aujordhui_date = new Date();
+  var aujordhui= aujordhui_date.getDate();
+  var ce_mois = aujordhui_date.getMonth()+1;
+  var cette_annee=aujordhui_date.getFullYear();
+  var date = new Date(date);
+  var dd = date.getDate();
+  var mm = date.getMonth() + 1;
+  var yyyy = date.getFullYear();
+  var day = date.getDay();
+
+  let date_rdv=(mm*10)+dd;
+  let date_courante=(ce_mois*10)+aujordhui;
+
+  var validacion = parseInt(mm*10)+parseInt(date_rdv)>parseInt(ce_mois*10)+parseInt(date_courante+3);
+  console.log("validation"+validacion);
+  if(day == 0 || day == 1  ){//si c' est dimanche = 0 ou lundi = 1
+    return false
+  }else {
+    if(mm>ce_mois || yyyy>cette_annee){ //valeur absolue du dernier jour du mois - aujordhui
+      return true
+    }else if(mm<ce_mois) {
+      return false;
+    }else if(mm==ce_mois) {
+      if(validacion==true){
+        return true;
+      }else {
+        return false;
+      }
+    }
+  }
+}
